01e12c6186be3d88ddc0eed9280f0d57488e9b79
iquhsek <shuqike.research@gmail.com>
2023-08-27 15:42:25 -0400

    Go

---
+++ planning_step4_7b.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0,1 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
+--model_name Vicuna \
+--name planning_step4_7b \
+--data data/blocksworld/step_4.json \
+--horizon 4 \
+--search_depth 2 \
+--alpha 0.5 \
+--sample_per_node 2 \
+--model_path lmsys/vicuna-7b-v1.3 \
+--num_gpus 2
