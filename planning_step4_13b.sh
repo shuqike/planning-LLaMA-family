daf786f45687e0c051ec93055ded602a0aab991f
iquhsek <shuqike.research@gmail.com>
2023-08-27 21:54:40 -0400

    Update planning_step4_13b.sh

--- planning_step4_13b.sh
+++ planning_step4_13b.sh
@@ -1,11 +1,11 @@
-# id: 2497224
+# id: 
 CUDA_VISIBLE_DEVICES=7,8,9 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step4_13b \
 --data data/blocksworld/step_4.json \
 --horizon 4 \
 --search_depth 2 \
---alpha 0.3 \
+--alpha 0 \
 --sample_per_node 2 \
 --model_path lmsys/vicuna-13b-v1.3 \
 --num_gpus 3
