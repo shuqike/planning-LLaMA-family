9b27e8af9456207d498d19266f1a6e6d3521e49b
iquhsek <shuqike.research@gmail.com>
2023-08-30 15:58:29 -0400

    Go

---
+++ planning_step6_13b.sh
@@ -0,0 +1,11 @@
+# id: 
+CUDA_VISIBLE_DEVICES=7,8,9 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
+--model_name Vicuna \
+--name planning_step6_13b \
+--data data/blocksworld/step_6.json \
+--horizon 6 \
+--search_depth 3 \
+--alpha 0 \
+--sample_per_node 2 \
+--model_path lmsys/vicuna-13b-v1.3 \
+--num_gpus 3
