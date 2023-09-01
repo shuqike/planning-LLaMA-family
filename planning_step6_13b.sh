428162b509a83c2f5f77baf8ba823ccf18f4df4d
iquhsek <shuqike.research@gmail.com>
2023-09-01 14:10:48 -0400

    Go and do serd1samp1

--- planning_step6_13b.sh
+++ planning_step6_13b.sh
@@ -1,12 +1,11 @@
-# id: 3176360
+# id: 
 CUDA_VISIBLE_DEVICES=7,8,9 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step6_13b \
 --data data/blocksworld/step_6.json \
 --horizon 6 \
---search_depth 3 \
+--search_depth 1 \
 --alpha 0 \
---sample_per_node 2 \
+--sample_per_node 1 \
 --model_path lmsys/vicuna-13b-v1.3 \
---num_gpus 3 \
---resume_file_idx 73
+--num_gpus 3
