0af3aed3323214672c1fa5acc1f8082f7273fd05
iquhsek <shuqike.research@gmail.com>
2023-09-04 16:46:00 -0400

    Rearranged logs

--- scripts/pl4_serd1_samp1/planning_step4_13b.sh
+++ scripts/pl4_serd1_samp1/planning_step4_13b.sh
@@ -1,4 +1,4 @@
-# id: 
+# id: 736212
 CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step4_13b \
@@ -8,5 +8,4 @@ CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 2
 --alpha 0 \
 --sample_per_node 1 \
 --model_path lmsys/vicuna-13b-v1.3 \
---num_gpus 3 \
---use_lang_goal
+--num_gpus 3
