8ea8ceaaf3bdee3afe3a691440bda8968b171661
shuqike <shuqike.research@gmail.com>
2023-09-03 20:04:14 -0500

    mcts step6 done

--- planning_step6_13b.sh
+++ planning_step6_13b.sh
@@ -1,5 +1,5 @@
 # id: 3621966
-CUDA_VISIBLE_DEVICES=7,8,9 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
+CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step6_13b \
 --data data/blocksworld/step_6.json \
@@ -8,4 +8,5 @@ CUDA_VISIBLE_DEVICES=7,8,9 nohup python -m torch.distributed.run --master_port 2
 --alpha 0 \
 --sample_per_node 1 \
 --model_path lmsys/vicuna-13b-v1.3 \
---num_gpus 3
+--num_gpus 3 \
+--resume_file_idx 58
