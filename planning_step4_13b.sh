b55d8809a793043de3d7033acd6c86ba117ef0a1
iquhsek <shuqike.research@gmail.com>
2023-09-04 14:46:35 -0400

    Placed two scripts for run

--- planning_step4_13b.sh
+++ planning_step4_13b.sh
@@ -1,9 +1,9 @@
 # id: 
 CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
---name planning_step6_13b \
---data data/blocksworld/step_6.json \
---horizon 6 \
+--name planning_step4_13b \
+--data data/blocksworld/step_4.json \
+--horizon 4 \
 --search_depth 1 \
 --alpha 0 \
 --sample_per_node 1 \
