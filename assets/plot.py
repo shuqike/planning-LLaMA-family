601302fcda97535500c757239e882913a354931f
iquhsek <shuqike.research@gmail.com>
2023-09-07 00:10:24 +0800

    Go

--- assets/plot.py
+++ assets/plot.py
@@ -18,16 +18,22 @@ my_cmap = mpl.colormaps['tab20']
 # for blocksworld step4 vicuna13B rulegoal
 sample_x = [232, 856,1446,3288,11444,14250,17100]
 success_rate_y = [93,100,100,100,100,100,100]
-plt.plot(sample_x, success_rate_y, color='y', marker='o', markersize=9, linestyle='dashed', label='Rule-based subgoal setting')
-sample_x = [232, 1434]
-success_rate_y = [43.86, 73.68]
-plt.plot(sample_x, success_rate_y, color='b', marker='o', markersize=9, linestyle='dashed', label='LLM-based subgoal setting')
-# sample_x = [228,439,856,1446,3288,11444,14250,17100]
-# success_rate_y = [12.28,15.79,36.84,47.37,64.91,100,100,100]
-# plt.plot(sample_x, success_rate_y, color='b', label='Rough subgoal setting')
-sample_x = [17100,14250,11400,10260,7980,5700,3705,3420,3135,2850,2565,2280,1995,1710,1425,1140,855,570,285]
-success_rate_y = [100,98,94.74,94.74,92.98,92.98,91.23,91.23,89.47,87.72,84.21,77.19,66.67,50.88,43.86,29.82,22.81,12.28,12.28]
+plt.plot(sample_x, success_rate_y, color='y', marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
+'''---------'''
+sample_x = [232, 1434, 3402]
+success_rate_y = [43.86, 73.68, 84.21]
+plt.plot(sample_x, success_rate_y, color='b', marker='o', markersize=9, linestyle='dashed', label='[Domain]LLM-based subgoal')
+'''---------'''
+sample_x = [228,439,856,1446,3288,11444,14250,17100]
+success_rate_y = [12.28,15.79,36.84,47.37,64.91,100,100,100]
+plt.plot(sample_x, success_rate_y, color='purple', marker='o', markersize=9, linestyle='dashed', label='[Rough]Rule-based subgoal')
+'''---------'''
+# sample_x = [17100,14250,11400,10260,7980,5700,3705,3420,3135,2850,2565,2280,1995,1710,1425,1140,855,570,285] # wrong stat of mcts4
+# success_rate_y = [100,98,94.74,94.74,92.98,92.98,91.23,91.23,89.47,87.72,84.21,77.19,66.67,50.88,43.86,29.82,22.81,12.28,12.28] # wrong stat of mcts4
+sample_x = [17100, 15960, 15105, 10260, 7410, 5700, 5130, 2280, 1140, 570]
+success_rate_y = [89, 86, 85.5, 82.99, 75.3, 74.7, 67.5, 57, 39.47, 23.68]
 plt.plot(sample_x, success_rate_y, color='r', marker='o', markersize=9, linestyle='dashed', label='MCTS')
+'''---------'''
 plt.ylabel('Success rate (%)')
 plt.xlabel('Sample')
 plt.title('Blocksworld Step4 Vicuna-13B')
@@ -35,14 +41,14 @@ plt.legend()
 plt.show()
 
 # for blocksworld step6 vicuna13B rulegoal
-# plt.title('Blocksworld Step6 Vicuna-13B')
-# sample_x = [728, 4621, 10591]
-# success_rate_y = [61.4, 79.8, 99.1]
-# plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='Our method')
-# sample_x = [1596, 2394, 3990, 4788, 5586, 6384, 7182, 7980, 8778, 9576, 15960, 28728, 39900, 47880] # treat multiple trials like one-shot
-# success_rate_y = [0,  0, 6.1, 7.9, 10.5, 15.7, 16.6, 19.3, 22.8, 26.3, 37.7, 53.5, 61.4, 71.9] # treat multiple trials like one-shot
-# plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='MCTS')
-# plt.ylabel('Success rate (%)')
-# plt.xlabel('Sample')
-# plt.legend()
-# plt.show()
+plt.title('Blocksworld Step6 Vicuna-13B')
+sample_x = [728, 4621, 10591]
+success_rate_y = [61.4, 79.8, 99.1]
+plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
+sample_x = [1596, 2394, 3990, 4788, 5586, 6384, 7182, 7980, 8778, 9576, 15960, 28728, 39900, 47880] # treat multiple trials like one-shot
+success_rate_y = [0,  0, 6.1, 7.9, 10.5, 15.7, 16.6, 19.3, 22.8, 26.3, 37.7, 53.5, 61.4, 71.9] # treat multiple trials like one-shot
+plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='MCTS')
+plt.ylabel('Success rate (%)')
+plt.xlabel('Sample')
+plt.legend()
+plt.show()
