46fb58d4f389b2f7171222d492fdc0f3cd12d31e
shuqike <shuqike.research@gmail.com>
2023-09-06 20:32:34 -0500

    planning llmsubgoal step4 serd2 samp-all

--- planning_step6_13b.sh
+++ planning_step6_13b.sh
@@ -1,5 +1,5 @@
 # id: 
-CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_planning.py \
+CUDA_VISIBLE_DEVICES=3,4,5 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step6_13b \
 --data data/blocksworld/step_6.json \
