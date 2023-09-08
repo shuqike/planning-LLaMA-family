612bad80a8bd5277701b1bfa5cce06abf8533d4d
iquhsek <shuqike.research@gmail.com>
2023-09-08 15:26:47 -0400

    Go

--- get_port.py
+++ get_port.py
@@ -3,8 +3,8 @@ import socket
 
 def find_free_port():
     with socket.socket() as s:
-            s.bind(('', 0))
-    return s.getsockname()[1]
+        s.bind(('', 0))
+        return s.getsockname()[1]
 
 
 if __name__ == '__main__':
---
+++ rafa_step4_13b.sh
@@ -0,0 +1,2 @@
+# id: 
+CUDA_VISIBLE_DEVICES=6,7,8 nohup python -m torch.distributed.run --master_port 
