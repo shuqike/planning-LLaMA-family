18134f4a68196bb538ace5dd0f87b60a05ec6476
iquhsek <shuqike.research@gmail.com>
2023-09-08 22:30:14 -0400

    Create rafa_step4_13b.sh

---
+++ rafa_step4_13b.sh
@@ -0,0 +1,13 @@
+# id: 
+CUDA_VISIBLE_DEVICES=6,7,8 nohup python -m torch.distributed.run --master_port 57271 --nproc_per_node 1 run_rafa.py \
+--model_name Vicuna \
+--name rafa_step4_13b \
+--data data/blocksworld/step_4.json \
+--n_trials 3 \
+--horizon 4 \
+--search_depth 2 \
+--alpha 0 \
+--sample_per_node 2 \
+--model_path lmsys/vicuna-13b-v1.3 \
+--num_gpus 3 \
+--use_lang_goal
