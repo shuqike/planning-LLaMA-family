d937e12e8620812ffa2e0f8f5ee77ffc0fc14172
iquhsek <shuqike.research@gmail.com>
2023-09-10 00:29:37 -0400

    rearranged scripts and logs

--- assets/plot.py
+++ assets/plot.py
@@ -20,8 +20,8 @@ sample_x = [232, 856,1446,3288,11444,14250,17100]
 success_rate_y = [93,100,100,100,100,100,100]
 plt.plot(sample_x, success_rate_y, color='y', marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
 '''---------'''
-sample_x = [232, 1434, 3187, 3402, 11832]
-success_rate_y = [43.86, 73.68, 80.7, 84.21, 100]
+sample_x = [232, 683, 1434, 3187, 3402, 11832]
+success_rate_y = [43.86, 43.86, 73.68, 80.7, 84.21, 100]
 plt.plot(sample_x, success_rate_y, color='b', marker='o', markersize=9, linestyle='dashed', label='[Domain]LLM-based subgoal')
 '''---------'''
 sample_x = [228,439,856,1446,3288,11444,14250,17100]
@@ -51,6 +51,21 @@ plt.plot(sample_x, success_rate_y, color=my_cmap(12), marker='o', markersize=9, 
 sample_x = [1596, 2394, 3990, 4788, 5586, 6384, 7182, 7980, 8778, 9576, 15960, 28728, 39900, 47880] # treat multiple trials like one-shot
 success_rate_y = [0,  0, 6.1, 7.9, 10.5, 15.7, 16.6, 19.3, 22.8, 26.3, 37.7, 53.5, 61.4, 71.9] # treat multiple trials like one-shot
 plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='MCTS')
+plt.ylabel('Success rate (%)')
+plt.xlabel('Sample')
+plt.legend()
+plt.show()
+
+# rafa step4 vicuna13B
+plt.title('Blocksworld Step4 Vicuna-13B')
+'''---------'''
+sample_x = [17100, 15960, 15105, 10260, 7410, 5700, 5130, 2280, 1140, 570]
+success_rate_y = [89, 86, 85.5, 82.99, 75.3, 74.7, 67.5, 57, 39.47, 23.68]
+plt.plot(sample_x, success_rate_y, color='r', marker='o', markersize=9, linestyle='dashed', label='MCTS')
+'''---------'''
+sample_x = [456, 684]
+success_rate_y = [71.9, 71.9]
+plt.plot(sample_x, success_rate_y, color='b', marker='o', markersize=9, linestyle='dashed', label='MPC')
 plt.ylabel('Success rate (%)')
 plt.xlabel('Sample')
 plt.legend()
