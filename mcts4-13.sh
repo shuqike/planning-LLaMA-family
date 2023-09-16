4dd16ef0c2d872f88721a71ad5c6d7b10f503f5e
iquhsek <shuqike.research@gmail.com>
2023-09-15 22:46:11 -0400

    Update mcts4-13.sh

--- mcts4-13.sh
+++ mcts4-13.sh
@@ -1,6 +1,7 @@
+# id: 
 for rollouts in 2 3 4 5 6 7 8 9 10 11 12 13
 do
-    CUDA_VISIBLE_DEVICES=8,9 nohup python -m torch.distributed.run --master_port 34471 --nproc_per_node 1 run_blocksworld.py --task mcts --model_name Vicuna --verbose False \
+    CUDA_VISIBLE_DEVICES=8,9 nohup python -m torch.distributed.run --master_port 34471 --nproc_per_node 1 run_mcts.py --task mcts --model_name Vicuna --verbose False \
     --data data/blocksworld/step_4.json \
     --max_depth 4 \
     --name mcts4rest${rollouts} \
