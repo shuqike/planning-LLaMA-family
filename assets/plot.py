880a542e0a05c42984081fe693b199d5f9a8d211
iquhsek <shuqike.research@gmail.com>
2023-09-20 15:30:05 -0400

    go

--- assets/plot.py
+++ assets/plot.py
@@ -62,19 +62,49 @@ plt.show()
 # rafa step4 vicuna13B
 plt.title('Blocksworld Step4 Vicuna-13B')
 '''---------'''
-sample_x = [59, 57, 52, 43, 37, 30, 26, 19, 15, 12, 10, 8, 6, 5, 4, 3, 2]
-success_rate_y = [86.9759, 86.5, 85.3, 82.9, 80.986, 77.77, 75.3, 68.79, 62.8, 55.7, 48.77, 39.47, 28.65, 24.21, 19.298, 15.789, 12.28]
-plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='MCTS')
-'''---------'''
-sample_x = [2, 3]
-success_rate_y = [71.9, 71.9]
-plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='MPC-bfs(b=2)')
-'''---------'''
 sample_x = [2, 3, 4, 5, 6, 8, 10, 12, 15, 19, 26, 30, 37, 43, 52, 57, 59]
 success_rate_y = [22.8, 30.99, 36.4, 42.8, 48.2, 57.89, 64.9, 70.175, 75.43, 79.87, 84.5, 86.6, 89.14, 90.65687, 92.27, 92.95, 93.19]
 # success_rate_y = np.array(success_rate_y) - 22.8 + 12.28
-plt.plot(sample_x, success_rate_y, color=my_cmap(12), marker='o', markersize=9, linestyle='dashed', label='MPC-mcts(s=1)')
-plt.ylabel('Success rate (%)')
-plt.xlabel('Trials')
-plt.legend()
+plt.plot(sample_x, success_rate_y, color=my_cmap(12), marker='o', markersize=9, linestyle='dashed', label='RAFA')
+'''---------'''
+sample_x = [59, 57, 52, 43, 37, 30, 26, 19, 15, 12, 10, 8, 6, 5, 4, 3, 2]
+success_rate_y = [86.9759, 86.5, 85.3, 82.9, 80.986, 77.77, 75.3, 68.79, 62.8, 55.7, 48.77, 39.47, 28.65, 24.21, 19.298, 15.789, 12.28]
+plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='MCTS')
+# '''---------'''
+# sample_x = [2, 3]
+# success_rate_y = [71.9, 71.9]
+# plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='MPC-bfs(b=2)')
+plt.ylim(-2, 100)
+plt.ylabel('Success Rate (%)')
+plt.xlabel('Episode')
+plt.legend(loc='lower right')
+plt.show()
+
+# rafa step4 vicuna33B
+plt.title('Blocksworld Step4 Vicuna-33B')
+'''---------'''
+sample_x = [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 19, 26, 30, 37, 43, 52, 57, 59]
+success_rate_y = [43.8596, 53.5, 62.57, 68.42, 72.98, 76.3, 80.7, 83.5, 85.38, 87.6, 89.658, 91.97, 92.8, 94.12, 94.94, 95.8, 96.18, 96.3128]
+plt.plot(sample_x, success_rate_y, color=my_cmap(12), marker='o', markersize=9, linestyle='dashed', label='RAFA')
+'''---------'''
+sample_x = [1]
+success_rate_y = [63]
+plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='d', markersize=9, linestyle='dashed', label='CoT(gpt-4)')
+'''---------'''
+sample_x = [1]
+success_rate_y = [2]
+plt.plot(sample_x, success_rate_y, color=my_cmap(2), marker='d', markersize=9, linestyle='dashed', label='CoT')
+'''---------'''
+sample_x = [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 19, 26, 30, 37, 43, 52, 57, 59]
+success_rate_y = [21, 23.68, 33.33, 39.47, 45.26, 50.29, 57.456, 62.1, 65.789, 69.94, 73.96, 78.6, 80.526, 83.357, 85.4, 87.7, 88.79655, 89.17633]
+plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='MCTS')
+'''---------'''
+plt.axvline(x=1, color='black', linestyle='-.', alpha=0.2)
+plt.axhline(y=2, color='black', linestyle='-.', alpha=0.2)
+plt.axhline(y=63, color='black', linestyle='-.', alpha=0.2)
+'''---------'''
+plt.ylim(-2, 100)
+plt.ylabel('Success Rate (%)')
+plt.xlabel('Episode')
+plt.legend(loc='lower right')
 plt.show()
---
+++ logs/mcts-rafm_step4_33b_try60/extract.py
@@ -0,0 +1,17 @@
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_trial', type=int, default=0)
+args = parser.parse_args()
+num_trial = args.num_trial
+
+
+succ = 0
+for i in range(57):
+    f = open("./json/" + "{:04d}".format(i) + ".json")
+    data = json.load(f)
+    for j in range(num_trial):
+        succ += int(data[j]["correct"])
+print(succ / 57 / num_trial)
