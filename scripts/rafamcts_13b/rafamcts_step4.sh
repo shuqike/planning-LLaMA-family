b38ca0b8da39f8601610a37f0549a5a1a8a7e06c
iquhsek <shuqike.research@gmail.com>
2023-09-20 10:26:44 -0400

    rearranged scripts

--- scripts/rafamcts_13b/rafamcts_step4.sh
+++ scripts/rafamcts_13b/rafamcts_step4.sh
@@ -1,4 +1,4 @@
-# id: 
+# id: 873733
 for rollouts in 100
 do
     CUDA_VISIBLE_DEVICES=6,7,8 nohup python -m torch.distributed.run --master_port 57271 --nproc_per_node 1 run_rafa_mcts.py --model_name Vicuna --verbose False --data data/blocksworld/step_4.json --max_depth 4 --name rafm_step4_13b_try${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-13b-v1.3 --num_gpus 3
