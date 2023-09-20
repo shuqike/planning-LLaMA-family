5c8c5b7234fa9476e25be3a78d438ed94c2c834b
iquhsek <shuqike.research@gmail.com>
2023-09-20 10:26:48 -0400

    Update rafamcts_step6_33b.sh

--- rafamcts_step6_33b.sh
+++ rafamcts_step6_33b.sh
@@ -2,5 +2,5 @@
 # cuda-pid: 
 for rollouts in 60
 do
-    CUDA_VISIBLE_DEVICES=4,5,6 nohup python -m torch.distributed.run --master_port 36977 --nproc_per_node 1 run_rafa_mcts.py --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name rafm_step6_33b_try${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 3
+    CUDA_VISIBLE_DEVICES=4,5,6 nohup python -m torch.distributed.run --master_port 36977 --nproc_per_node 1 run_rafa_mcts.py --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name rafm_step6_33b_try${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 3 --resume_file_idx 8
 done
