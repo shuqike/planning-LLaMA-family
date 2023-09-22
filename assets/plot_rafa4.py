504f8d4a0ee738b7c44d8cc6439819988e8ac6a7
iquhsek <shuqike.research@gmail.com>
2023-09-22 15:17:48 -0400

    go

--- assets/plot_rafa4.py
+++ assets/plot_rafa4.py
@@ -9,8 +9,8 @@ plt.rc('text', usetex=True)
 plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
 
 
-# rafa step6 vicuna 13B
-plt.title('Blocksworld Step6 Vicuna-13B', size=30)
+# rafa step6 vicuna 33B
+plt.title('Blocksworld Step6 Vicuna-33B', size=30)
 fig = matplotlib.pyplot.gcf()
 fig.set_size_inches(16, 10)
 ax.spines['right'].set_visible(False)
@@ -25,7 +25,7 @@ ax.xaxis.set_major_locator(ticker.MaxNLocator(6))
 ax.yaxis.set_major_locator(ticker.MaxNLocator(6))
 '''---------'''
 sample_x = [1, 3, 4, 6, 8, 10, 13, 16, 20, 23, 27, 32, 36, 42, 48, 55, 60]
-success_rate_y = [0, 3.8, 7, 12.865, 18.53, 23.3, 28.87989, 34, 40, 43.8, 48, 53, 56.676, 61, 64.6, 68, 70]
+success_rate_y = [14, 23.9766, 27.85, 33.33, 37.28, 40.877, 45.7, 49.89, 54, 56.598, 59.55, 62.7, 64.9, 67.75, 70.23, 72.8, 74.4]
 ax.plot(sample_x, success_rate_y, color=color_list[0], linewidth=3, label='RAFA')
 '''---------'''
 sample_x = [1]
@@ -33,19 +33,11 @@ success_rate_y = [40]
 ax.plot(sample_x, success_rate_y, color=color_list[1], linewidth=3, label='CoT(gpt-4)')
 ax.axhline(y=40, color=color_list[1], linewidth=3, linestyle='-.', alpha=0.9)
 '''---------'''
-# sample_x = [1, 3, 4, 6, 8, 10, 13, 16, 20, 23, 27, 32, 36, 42, 48, 55, 60]
-# success_rate_y = [0, 0, 1, 3, 5.59, 8, 12, 15.296, 19.47, 22.27, 25.666, 29.44, 32, 35.359, 38.3, 41, 43.845]
-# plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='MCTS')
-ax.plot([1], [8], color=color_list[2], linewidth=3, label=r'$\texttt{RAP}^{(10)}$')
-ax.axhline(y=8, color=color_list[2], linewidth=3, linestyle='-.', alpha=0.9)
-ax.plot([1], [19.47], color=color_list[3], linewidth=3, label=r'$\texttt{RAP}^{(20)}$')
-ax.axhline(y=19.47, color=color_list[3], linewidth=3, linestyle='-.', alpha=0.9)
-'''---------'''
 sample_x = [1]
 success_rate_y = [0]
-ax.plot(sample_x, success_rate_y, color=color_list[4], linewidth=3, label='CoT(LLaMA-33B)')
-ax.axhline(y=0, color=color_list[4], linewidth=3, linestyle='-.', alpha=0.9)
+ax.plot(sample_x, success_rate_y, color=color_list[2], linewidth=3, label='CoT(LLaMA-33B)')
+ax.axhline(y=0, color=color_list[2], linewidth=3, linestyle='-.', alpha=0.9)
 '''---------'''
 plt.ylim(-2, 100)
 legend()
-plt.savefig('step6_13b.pdf', format='pdf')
+plt.savefig('step6_33b.pdf', format='pdf')
