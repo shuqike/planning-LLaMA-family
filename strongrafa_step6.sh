d861e1f6585e399d40ffebfe181ee9ef90b88c90
iquhsek <shuqike.research@gmail.com>
2023-09-22 01:25:59 -0400

    Update strongrafa_step6.sh

--- strongrafa_step6.sh
+++ strongrafa_step6.sh
@@ -2,5 +2,5 @@
 # cuda id: 
 for rollouts in 60
 do
-    CUDA_VISIBLE_DEVICES=6,7,8,9 nohup python -m torch.distributed.run --master_port 54543 --nproc_per_node 1 run_srm.py --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name srm_step6_33b_try${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 4
+    CUDA_VISIBLE_DEVICES=5,6,7,8,9 nohup python -m torch.distributed.run --master_port 54543 --nproc_per_node 1 run_srm.py --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name srm_step6_33b_try${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 5
 done
