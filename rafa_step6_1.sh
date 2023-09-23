6e9adb6e762fed912f04c812e0b55c4e39c9bc28
iquhsek <shuqike.research@gmail.com>
2023-09-22 20:33:46 -0400

    go

---
+++ rafa_step6_1.sh
@@ -0,0 +1,15 @@
+# id: 
+# cuda id: 
+CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 57271 --nproc_per_node 1 run_rafa.py \
+--model_name Vicuna \
+--name rafa_step6_33b \
+--data data/blocksworld/step_6.json \
+--n_trials 59 \
+--horizon 6 \
+--search_depth 2 \
+--alpha 0 \
+--sample_per_node 2 \
+--model_path lmsys/vicuna-33b-v1.3 \
+--num_gpus 3 \
+--use_lang_goal \
+--use_mem_prompt
---
+++ rafa_step6_3.sh
@@ -0,0 +1,16 @@
+# id: 
+# cuda id: 
+CUDA_VISIBLE_DEVICES=6,7,8 nohup python -m torch.distributed.run --master_port 54543 --nproc_per_node 1 run_rafa.py \
+--model_name Vicuna \
+--name rafa_step6_33b \
+--data data/blocksworld/step_6.json \
+--n_trials 59 \
+--horizon 6 \
+--search_depth 2 \
+--alpha 0 \
+--sample_per_node 2 \
+--model_path lmsys/vicuna-33b-v1.3 \
+--num_gpus 3 \
+--use_lang_goal \
+--use_mem_prompt \
+--resume_file_idx 80
