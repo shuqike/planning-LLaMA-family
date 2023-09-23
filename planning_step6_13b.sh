007a7d5c5c49d7d8808db866b53d0e539ee9cbd4
iquhsek <shuqike.research@gmail.com>
2023-09-22 20:26:27 -0400

    go

--- planning_step6_13b.sh
+++ planning_step6_13b.sh
@@ -1,4 +1,5 @@
 # id: 
+# cuda id: 
 CUDA_VISIBLE_DEVICES=3,4,5 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step6_13b \
@@ -10,4 +11,4 @@ CUDA_VISIBLE_DEVICES=3,4,5 nohup python -m torch.distributed.run --master_port 1
 --model_path lmsys/vicuna-13b-v1.3 \
 --num_gpus 3 \
 --use_lang_goal \
---resume_file_idx 85
+--resume_file_idx 92
--- planning_step6_13b_par.sh
+++ planning_step6_13b_par.sh
@@ -1,4 +1,5 @@
 # id: 
+# cuda id: 
 CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 29400 --nproc_per_node 1 run_planning.py \
 --model_name Vicuna \
 --name planning_step6_13b_par \
@@ -10,4 +11,4 @@ CUDA_VISIBLE_DEVICES=0,1,2 nohup python -m torch.distributed.run --master_port 2
 --model_path lmsys/vicuna-13b-v1.3 \
 --num_gpus 3 \
 --use_lang_goal \
---resume_file_idx 76
+--resume_file_idx 82
