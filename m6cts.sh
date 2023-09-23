23c1b370e9484b4db64e2f372e49517dfa09a0df
iquhsek <shuqike.research@gmail.com>
2023-09-22 21:00:27 -0400

    Update m6cts.sh

--- m6cts.sh
+++ m6cts.sh
@@ -1,4 +1,6 @@
+# id: 
+# cuda id: 
 for rollouts in 60
 do
-    CUDA_VISIBLE_DEVICES=9,8,7 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_blocksworld.py --task mcts --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name m6ct_roll${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 3
+    CUDA_VISIBLE_DEVICES=9,8,7 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_mcts.py --task mcts --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name m6ct_roll${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 3
 done
