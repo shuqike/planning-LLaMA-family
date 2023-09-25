73f26f96c32518001814b25fe6480ff22d85d392
iquhsek <shuqike.research@gmail.com>
2023-09-25 00:32:42 -0400

    go

--- assets/plot_planning.py
+++ assets/plot_planning.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from plot_utils import *
 plt.rc('text', usetex=True)
 plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
 my_cmap = mpl.colormaps['tab20']
@@ -22,44 +21,45 @@ my_cmap = mpl.colormaps['tab20']
 # plt.show()
 
 # for blocksworld step4 vicuna13B rulegoal
-sample_x = [232, 856,1446,3288,11444,14250,17100]
-success_rate_y = [93,100,100,100,100,100,100]
-plt.plot(sample_x, success_rate_y, color='y', marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
+# sample_x = [232, 856,1446,3288,11444,14250,17100]
+# success_rate_y = [93,100,100,100,100,100,100]
+# plt.plot(sample_x, success_rate_y, color='y', marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
 '''---------'''
-sample_x = [232, 683, 1434, 3187, 3402, 11832]
-success_rate_y = [43.86, 43.86, 73.68, 80.7, 84.21, 100]
-plt.plot(sample_x, success_rate_y, color='b', marker='o', markersize=9, linestyle='dashed', label='[Domain]LLM-based subgoal')
+sample_x = [232, 683, 1434, 3187, 3402, 11832, 14820, 16815]
+success_rate_y = [43.86, 43.86, 73.68, 80.7, 84.21, 100, 100, 100]
+plt.plot(sample_x, success_rate_y, color='b', marker='o', markersize=9, linestyle='dashed', label='PvS')
 '''---------'''
-sample_x = [228,439,856,1446,3288,11444,14250,17100]
-success_rate_y = [12.28,15.79,36.84,47.37,64.91,100,100,100]
-plt.plot(sample_x, success_rate_y, color='purple', marker='o', markersize=9, linestyle='dashed', label='[Rough]Rule-based subgoal')
+# sample_x = [228,439,856,1446,3288,11444,14250,17100]
+# success_rate_y = [12.28,15.79,36.84,47.37,64.91,100,100,100]
+# plt.plot(sample_x, success_rate_y, color='purple', marker='o', markersize=9, linestyle='dashed', label='[Rough]Rule-based subgoal')
 '''---------'''
 sample_x = [16815, 16245, 14820, 12255, 10545, 8550, 7410, 5415, 4275, 3420, 2850, 2280, 1425, 1140, 855, 570]
 success_rate_y = [86.9759, 86.5, 85.3, 82.9, 80.986, 77.77, 75.3, 68.79, 62.8, 55.7, 48.77, 39.47, 24.21, 19.298, 15.789, 12.28]
-plt.plot(sample_x, success_rate_y, color='r', marker='o', markersize=9, linestyle='dashed', label='MCTS')
+plt.plot(sample_x, success_rate_y, color='r', marker='o', markersize=9, linestyle='dashed', label='RAP')
 '''---------'''
-plt.ylabel('Success rate (%)')
+plt.ylabel('Success rate (\%)')
 plt.xlabel('Sample')
 plt.title('Blocksworld Step4 Vicuna-13B')
 plt.legend()
-plt.show()
+plt.savefig('plan_step4_13b.pdf', format='pdf')
 
 # for blocksworld step6 vicuna13B rulegoal
+plt.clf()
 plt.title('Blocksworld Step6 Vicuna-13B')
-sample_x = [728, 4621, 10591]
-success_rate_y = [61.4, 79.8, 99.1]
-plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
+# sample_x = [728, 4621, 10591]
+# success_rate_y = [61.4, 79.8, 99.1]
+# plt.plot(sample_x, success_rate_y, color=my_cmap(8), marker='o', markersize=9, linestyle='dashed', label='[Domain]Rule-based subgoal')
 '''partial notes
 bad pl6_serd3_sampall(38518, 57.89)
 good pl4_serd4_samp2(28739, 55.26)
 '''
 sample_x = [784, 5167, 13097, 28739]
 success_rate_y = [12.28, 26.3, 42.98, 55.26]
-plt.plot(sample_x, success_rate_y, color=my_cmap(12), marker='o', markersize=9, linestyle='dashed', label='[Domain]LLM-based subgoal')
+plt.plot(sample_x, success_rate_y, color=my_cmap(12), marker='o', markersize=9, linestyle='dashed', label='PvS')
 sample_x = [1596, 2394, 3990, 4788, 5586, 6384, 7182, 7980, 8778, 9576, 15960, 28728, 39900, 47880] # treat multiple trials like one-shot
 success_rate_y = [0,  0, 6.1, 7.9, 10.5, 15.7, 16.6, 19.3, 22.8, 26.3, 37.7, 53.5, 61.4, 71.9] # treat multiple trials like one-shot
-plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='MCTS')
-plt.ylabel('Success rate (%)')
+plt.plot(sample_x, success_rate_y, color=my_cmap(4), marker='o', markersize=9, linestyle='dashed', label='RAP')
+plt.ylabel('Success rate (\%)')
 plt.xlabel('Sample')
 plt.legend()
-plt.show()
+plt.savefig('plan_step6_13b.pdf', format='pdf')
--- m6cts.sh
+++ m6cts.sh
@@ -2,5 +2,5 @@
 # cuda id: 
 for rollouts in 60
 do
-    CUDA_VISIBLE_DEVICES=6,7,8 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_mcts.py --task mcts --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name m6ct_roll${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 3 --resume_file_idx 60
+    CUDA_VISIBLE_DEVICES=6,7,8 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_mcts.py --task mcts --model_name Vicuna --verbose False --data data/blocksworld/step_6.json --max_depth 6 --name m6ct_roll${rollouts} --rollouts $rollouts --model_path lmsys/vicuna-33b-v1.3 --num_gpus 3 --resume_file_idx 67
 done
